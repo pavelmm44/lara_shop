28d202fe924e428dcfce4de2c5d21876040fdb36
pavelmm44 <pavelmm44@gmail.com>
2025-04-20 18:00:18 +0300

    Add admin options

---
+++ app/Filament/Resources/OptionResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\OptionResource\Pages;
+use App\Filament\Resources\OptionResource\RelationManagers;
+use Domain\Product\Models\Option;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OptionResource extends Resource
+{
+    protected static ?string $model = Option::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-numbered-list';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->sortable()
+                    ->searchable()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOptions::route('/'),
+            'create' => Pages\CreateOption::route('/create'),
+            'edit' => Pages\EditOption::route('/{record}/edit'),
+        ];
+    }
+}
