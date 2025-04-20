82be1740aa5a5c1256aa7a97fb2bb503a10ac11a
pavelmm44 <pavelmm44@gmail.com>
2025-04-20 20:16:00 +0300

    Add home page

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Providers;
+
+use App\Contracts\RouteRegistrar;
+use App\Routing\AppRegistrar;
+use Domain\Auth\Routing\AuthRegistrar;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Contracts\Routing\Registrar;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+use RuntimeException;
+use Symfony\Component\HttpFoundation\Response;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    protected array $registrars = [
+        AppRegistrar::class,
+    ];
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->configureRateLimiting();
+
+        \Illuminate\Foundation\Support\Providers\RouteServiceProvider::loadRoutesUsing(
+        function (Registrar $router) {
+            $this->mapRoutes($router, $this->registrars);
+        });
+    }
+
+    protected function configureRateLimiting(): void
+    {
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(500)
+                ->by($request->user()?->id ?: $request->ip())
+                ->response(function (Request $request, array $headers) {
+                    return response('Take it easy', Response::HTTP_TOO_MANY_REQUESTS, $headers);
+                });
+        });
+
+        RateLimiter::for('auth', function (Request $request) {
+            return Limit::perMinute(20)
+                ->by($request->ip())
+                ->response(function (Request $request, array $headers) {
+                    return response('Take it easy', Response::HTTP_TOO_MANY_REQUESTS, $headers);
+                });
+        });
+    }
+
+    protected function mapRoutes(Registrar $router, array $registrars): void
+    {
+        foreach ($registrars as $registrar) {
+            if (!class_exists($registrar) || !is_subclass_of($registrar, RouteRegistrar::class)) {
+                throw new RuntimeException(sprintf(
+                    'Cannot map routes \'%s\', it is not a valid routes class',
+                    $registrar
+                ));
+            }
+
+            (new $registrar)->map($router);
+        }
+    }
+}
