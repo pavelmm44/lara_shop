adc64257571d1d781553145e7b59edf0eac7e8ad
pavelmm44 <pavelmm44@gmail.com>
2025-04-21 19:14:16 +0300

    Add GitHub user authentication

---
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Domain\Auth\Models\User;
+use DomainException;
+use Illuminate\Http\RedirectResponse;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialAuthController extends Controller
+{
+    public function redirect(string $driver): \Symfony\Component\HttpFoundation\RedirectResponse|RedirectResponse
+    {
+        try {
+            return Socialite::driver($driver)
+                ->redirect();
+        } catch (\Throwable $e) {
+            throw new DomainException('An error occurred or the driver is not supported');
+        }
+    }
+
+    public function callback(string $driver): RedirectResponse
+    {
+        if ($driver !== 'github') {
+            throw new DomainException('The driver is not supported');
+        }
+
+        $githubUser = Socialite::driver($driver)->user();
+
+        $user = User::query()->updateOrCreate([
+            $driver . '_id' => $githubUser->getId(),
+        ], [
+            'name' => $githubUser->getName(),
+            'email' => $githubUser->getEmail(),
+            'password' => bcrypt(str()->random(20))
+        ]);
+
+        auth()->login($user);
+
+        return redirect()->intended(route('home'));
+    }
+}
